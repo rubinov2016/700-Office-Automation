29d5a56e88c2c90b34f1c4f90fc9aef4c3da66a2
Ivan Rubinov <lrubinov@yandex.ru>
2024-02-18 17:09:08 +0000

    first commit

--- Copy_text_from_powerpoint.py
+++ Copy_text_from_powerpoint.py
@@ -23,9 +23,18 @@ def extract_text_from_folder(folder_path):
                 extracted_text.append(text)
     return '\n'.join(extracted_text)
 
-# Replace 'your_folder_path' with the path to your folder containing PowerPoint files
+def extract_text_from_folder(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        for file_name in files:
+            if file_name.endswith('.pptx'):
+                file_path = os.path.join(root, file_name)
+                text = extract_text_from_pptx_file(file_path)
+                # Create a text file with the same name as the PowerPoint file
+                text_file_path = os.path.splitext(file_path)[0] + '.txt'
+                with open(text_file_path, 'w', encoding='utf-8') as f:
+                    f.write(text)
+
 # folder_path = r'C:\Users\Lenovo\OneDrive - Solent University\Documents\724 AI in Business (Friday)\Lesson 1\'
-folder_path = r'C:\Users\Lenovo\OneDrive - Solent University\Documents\724 AI in Business (Friday)\Lesson 1'
-all_texts = extract_text_from_folder(folder_path)
-print(all_texts.encode('utf-8').decode(sys.stdout.encoding, 'ignore'))
+folder_path = r'C:\Users\Lenovo\OneDrive - Solent University\Documents\725 Analytics Visualisation (Tue)'
+extract_text_from_folder(folder_path)
 
